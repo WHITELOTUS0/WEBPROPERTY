72e2540f55745672975de7687d480af953ae1644
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-17 14:03:10 +0300

    Update addUser.js

--- addUser.js
+++ addUser.js
@@ -11,3 +11,20 @@ connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL database!');
 });
+
+const user = {
+    name: 'John Doe',
+    username: 'johndoe',
+    password: 'password123',
+    email: 'johndoe@example.com',
+    telno: '1234567890'
+  };
+
+connection.query('INSERT INTO user SET ?', user, (err, result) => {
+    if (err) throw err;
+    console.log('User added successfully!');
+    console.log('Inserted ID:', result.insertId);
+});
+
+
+  
