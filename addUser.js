e2519ea7df64edf710399396fb41d5411d79559e
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-17 15:20:39 +0300

    Update addUser.js

--- addUser.js
+++ addUser.js
@@ -1,63 +1,71 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('./db');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
-
-const user = {
-  Names: 'Glorry',
-  username: 'whitelotus',
-  password: 'password123',
-  email: 'glorry@example.com',
-  telno: '1234567890',
-};
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'manageProperty')));
 
 function checkEmailExists(email) {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM user WHERE email = ?';
-    db.query(query, [email], (error, results) => {
-      if (error) reject(error);
-      else resolve(results && results.length > 0);
-    });
-  });
-}
-
-checkEmailExists(user.email)
-  .then((emailExists) => {
-    if (emailExists) {
-      console.log('Email already exists. User not saved.');
-      return;
-    }
-
-    // Hash the password
-    bcrypt.hash(user.password, 10, (err, hashedPassword) => {
-      if (err) {
-        console.error('Error hashing password:', err);
-        return;
-      }
-
-      // Store the user in the database
-      const newUser = {
-        Names: user.Names,
-        username: user.username,
-        password: hashedPassword,
-        email: user.email,
-        telno: user.telno,
-      };
-
-      const query = 'INSERT INTO user SET ?';
-      db.query(query, newUser, (err, result) => {
-        if (err) {
-          console.error('Error adding user:', err);
-          return;
-        }
-        console.log('User added successfully!');
-        console.log('Result:', result);
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM user WHERE email = ?';
+      db.query(query, [email], (error, results) => {
+        if (error) reject(error);
+        else resolve(results && results.length > 0);
       });
     });
-  })
-  .catch((error) => {
-    console.error('Error checking email existence:', error);
+  }
+
+
+app.post('/register', (req, res) => {
+    const user = {
+      Names: req.body.Names,
+      password: req.body.password,
+      email: req.body.email,
+      telno: req.body.telno,
+    };
+  
+    checkEmailExists(user.email)
+      .then((emailExists) => {
+        if (emailExists) {
+          console.log('Email already exists. User not saved.');
+          res.send('Email already exists. User not saved.');
+          return;
+        }
+  
+        bcrypt.hash(user.password, 10, (err, hashedPassword) => {
+          if (err) {
+            console.error('Error hashing password:', err);
+            res.send('Error hashing password');
+            return;
+          }
+  
+          const newUser = {
+            Names: user.Names,
+            password: hashedPassword,
+            email: user.email,
+            telno: user.telno,
+          };
+  
+          const query = 'INSERT INTO user SET ?';
+          db.query(query, newUser, (err, result) => {
+            if (err) {
+              console.error('Error adding user:', err);
+              res.send('Error adding user');
+              return;
+            }
+            console.log('User added successfully!');
+            console.log('Result:', result);
+            res.send('User added successfully!');
+          });
+        });
+      })
+      .catch((error) => {
+        console.error('Error checking email existence:', error);
+        res.send('Error checking email existence');
+      });
   });
 
   app.listen(3000, () => {
